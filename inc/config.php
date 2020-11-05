a1e76633eeaed811cff72836a5b8463401c5c2c6
fabiansato <40687901+fabiansato@users.noreply.github.com>
2020-11-05 18:28:44 -0300

    Estructura web de php
    estructura web php

---
+++ inc/config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 
+ * @package  Estructura proyecto web
+ * @author   Fabian Sato <contacto@fabiansato.com>
+ * @license  GNU License
+ * @link     https://github.com/fabiansatocom
+ */
+define("SERVER","localhost");
+define("USER","user");
+define("PASS","pass");
+define("DB","db");
+
+//OTROS DATOS
+define("DEBUG","true");
+?>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+//Incluimos todas las clases y funciones del proyecto
+require_once 'inc/functions.php';
+
+//insertamos el header
+require_once 'mod/header.php';
+/*
+//Mostramos todos los datos de la tabla1
+$table1 = new Table1();
+print_r($table1->getAllInfo());
+
+//Mostramos todos los datos de la tabla2 que tenga como id = 2
+$tabla2 = new Table2();
+$table2->ID = 2;
+print_r($table2->getData());
+*/
+//Por último insertamos el footer
+require_once 'mod/footer.php';
