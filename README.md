67bbc4d78c616f32dfdc108ed1ff32ad8dce4689
fabiansato <40687901+fabiansato@users.noreply.github.com>
2020-11-05 18:56:05 -0300

    Actualizado el index.php
    agregado el index.php con datos

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # html-estructuraweb
- Estructura web en html (con etiquetas semanticas)
+ Estructura web en html con carpetas y estilos destinadas a generar el orden en los futuros proyectos.
+ 
+ Sumada la estructura semantica de html dentro del index.phps
--- index.php
+++ index.php
@@ -3,8 +3,15 @@
 //Incluimos todas las clases y funciones del proyecto
 require_once 'inc/functions.php';
 
-//insertamos el header
+
+
+    <header>
+	//insertamos el header
 require_once 'mod/header.php';
+	</header><!---cabecera-->
+    <nav></nav><!-- barra de navegacion-->
+    <section> <!-- inicio de los articulos-->
+        <article>
 /*
 //Mostramos todos los datos de la tabla1
 $table1 = new Table1();
@@ -15,5 +22,19 @@ $tabla2 = new Table2();
 $table2->ID = 2;
 print_r($table2->getData());
 */
+        </article>
+        <article>
+            
+        </article>
+        <article>
+            
+        </article>
+    </section>
+    <aside><!-- menu lateral-->
+
+    </aside>
+    <footer><!-- pie de pagina-->
+
 //Por último insertamos el footer
 require_once 'mod/footer.php';
+    </footer>
